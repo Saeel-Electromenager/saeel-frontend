1fd01b7922d9b248fb7c28a67b55142b76f07aa9
Yanis Oulhaci <yaniscrab@gmail.com>
2022-05-06 15:01:52 +0100

    structure des fichiers v1

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 ## Available Scripts
 
+<br>
+
+**After cloning the repository, you should run:**
+
+### `npm init`
+
+<br>
+
 In the project directory, you can run:
 
 ### `npm start`
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,96 @@
 import '../styles/Home.css';
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import AccountCircle from '@mui/icons-material/Lock';
+import Email from '@mui/icons-material/Email';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import Box from '@mui/material/Box';
 
 function Home() {
-  return <div className="App">Home page</div>;
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+  const login = (e) => {
+    e.preventDefault();
+    // TODO
+  };
+
+  return (
+    <div className="App">
+      <Box
+        onSubmit={login}
+        component="form"
+        autoComplete="off"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+      >
+        <TextField
+          sx={{ margin: '10px' }}
+          fullWidth
+          label="E-mail"
+          variant="outlined"
+          className="test"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Email />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <br />
+        <TextField
+          sx={{ margin: '10px' }}
+          label="Password"
+          type={values.showPassword ? 'text' : 'password'}
+          value={values.password}
+          onChange={handleChange('password')}
+          variant="outlined"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+        <br />
+        <Button type="submit" variant="contained">
+          Se connecter
+        </Button>
+      </Box>
+    </div>
+  );
 }
 
 export default Home;
